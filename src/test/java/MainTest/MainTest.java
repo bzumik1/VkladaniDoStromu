78c47564a016cb94962818a097610d456883765d
Jakub Znamenáček <jakub.znamenacek.fs.cvut>
2019-10-28 23:18:15 +0100

    S grafickým rozhraním a s testovací třídou

--- src/test/java/MainTest/MainTest.java
+++ src/test/java/MainTest/MainTest.java
@@ -1,36 +1,28 @@
-package znamenacek.jakub.fs.cvut.semestralniprace;
+package MainTest;
 
-  import javafx.application.Application;
-  import javafx.geometry.Pos;
-  import javafx.scene.Scene;
-  import javafx.scene.control.Button;
-  import javafx.scene.control.Label;
-  import javafx.scene.layout.StackPane;
-  import javafx.scene.layout.VBox;
-  import javafx.stage.Stage;
-
-import Algorithm.Node;
 import Algorithm.DataElement;
+import Algorithm.Node;
 import Algorithm.Tree;
+
 import java.util.ArrayList;
 
 /**
  * JavaFX App
  */
-public class App extends Application {
+public class MainTest{
 
-    
+
 
     public static void main(String[] args) {
         var n1 = new Node(new DataElement(3));
         n1.add(new DataElement(2));
         var aL = new ArrayList<Integer>();
         System.out.println(aL.size());
-        
+
         System.out.println("Priiiiiiiiint");
         System.out.println(n1);
-        
-        
+
+
         //TEST NODE
         System.out.println("TEST");
         System.out.println("CREATE TREE");
@@ -49,29 +41,17 @@ public class App extends Application {
         tree.add(new DataElement(15));
         tree.add(new DataElement(14));
         tree.add(new DataElement(12));
-        
-        
+
+
         //System.out.println(tree.getRoot().getChildren()[1].getChildren()[0].getSiblings()[0].nodeDataElemnetsToString());
-        
-        
+
+
         System.out.println("PRINT TREE");
         //System.out.println(tree.getRoot().getChildren()[0]);
         System.out.println(tree);
 
-        launch(args);
-        
+
     }
 
-    @Override
-    public void start(Stage stage) throws Exception {
-        var button = new Button("pokus");
-        var root = new VBox(10);
-        root.setAlignment(Pos.CENTER);
-        root.getChildren().addAll(root);
 
-        var scene = new Scene(root,600,600);
-        stage.setScene(scene);
-        stage.show();
-        stage.setTitle("pokus");
-     }
 }
