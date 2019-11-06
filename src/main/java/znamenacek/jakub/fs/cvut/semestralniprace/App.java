12d44343d1b43f5e1f69bcf76cc486a84f07fdfa
Jakub Znamenáček <jakub.znamenacek.fs.cvut>
2019-11-06 11:58:53 +0100

    Zcela fukční, nutno drobných úprav v kódu

--- src/main/java/znamenacek/jakub/fs/cvut/semestralniprace/App.java
+++ src/main/java/znamenacek/jakub/fs/cvut/semestralniprace/App.java
@@ -28,6 +28,8 @@ import java.util.ArrayList;
  */
 public class App extends Application {
     private static Tree tree;
+    private static int[]  dataElementArray ={3,7,4,9,10,0,5,6,8,2,1,20,15,14,12};
+    private int currentState =1;
 
     
 
@@ -45,6 +47,7 @@ public class App extends Application {
         System.out.println("TEST");
         System.out.println("CREATE TREE");
         var treeStatic = new Tree(new DataElement(3));
+        /*
         treeStatic.add(new DataElement(7));
         treeStatic.add(new DataElement(4));
         treeStatic.add(new DataElement(9));
@@ -59,6 +62,8 @@ public class App extends Application {
         treeStatic.add(new DataElement(15));
         treeStatic.add(new DataElement(14));
         treeStatic.add(new DataElement(12));
+
+         */
         
         tree = treeStatic;
         //System.out.println(tree.getRoot().getChildren()[1].getChildren()[0].getSiblings()[0].nodeDataElemnetsToString());
@@ -75,48 +80,75 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        //var treeGUI = new TreeGUI();
-        var text1 = new Label();
-        var square1 = new Square(100,2);
-        square1.setOnMouseDragged(e -> {
-                                            square1.setX(e.getX());
-                                            square1.setY(e.getY());
-        });
-        var square2 = new Square(100,2);
-        square2.setOnMouseDragged(e -> {
-                                            square2.setX(e.getX());
-                                            square2.setY(e.getY());
-        });
-
-        var line = new Line();
-        var line1 = new Line(0,0,100,100);
-        line.startXProperty().bind(square1.centerXProperty);
-        line.startYProperty().bind(square1.centerYProperty);
-        line.endXProperty().bind(square2.centerXProperty);
-        line.endYProperty().bind(square2.centerYProperty);
-        text1.setText("");
-
+        var treeGUI = new TreeGUI(tree.getRoot());
         var root = new VBox(20);
-        var vbox = new VBox(40);
-        var group = new Group();
-        group.maxWidth(300);
-        group.maxHeight(300);
+        var hBox = new HBox(10);
+        var nextButton = new Button ("další");
+        var previousButton = new Button ("předchozí");
+        var wholeButton = new Button("celý strom");
+        var startButton = new Button("start");
+
+        hBox.getChildren().addAll(startButton, wholeButton,nextButton,previousButton);
+        hBox.setAlignment(Pos.CENTER);
+
+        root.getChildren().addAll(treeGUI,hBox);
         root.setAlignment(Pos.CENTER);
 
-        vbox.getChildren().addAll(square1,square2);
-        group.getChildren().addAll(vbox,line1);
-        vbox.setLayoutX(100);
-        vbox.setLayoutY(100);
 
 
-        //root.getChildren().addAll(group);
+
+
+
 
 
 
         //var scene = new Scene(treeGUI.createTree(tree.getRoot()),600,600);
-        var scene = new Scene(new TreeGUI(tree.getRoot()),1200,600);
+        var scene = new Scene(root,1200,600);
         stage.setScene(scene);
         stage.show();
         stage.setTitle("pokus");
+
+
+
+        nextButton.setOnAction(e -> {
+            tree = new Tree(new DataElement(dataElementArray[0]));
+            currentState++;
+            for(int i=1;i< currentState;i++){
+                tree.add(new DataElement(dataElementArray[i]));
+            }
+            treeGUI.getChildren().clear();
+            treeGUI.getChildren().add(new TreeGUI(tree.getRoot()));
+
+        });
+
+        previousButton.setOnAction(e -> {
+            tree = new Tree(new DataElement(dataElementArray[0]));
+            currentState--;
+            for(int i=1;i< currentState;i++){
+                tree.add(new DataElement(dataElementArray[i]));
+            }
+            treeGUI.getChildren().clear();
+            treeGUI.getChildren().add(new TreeGUI(tree.getRoot()));
+
+        });
+
+        wholeButton.setOnAction(e -> {
+            tree = new Tree(new DataElement(dataElementArray[0]));
+            currentState = dataElementArray.length;
+            for(int i=1;i< currentState;i++){
+                tree.add(new DataElement(dataElementArray[i]));
+            }
+            treeGUI.getChildren().clear();
+            treeGUI.getChildren().add(new TreeGUI(tree.getRoot()));
+
+        });
+        startButton.setOnAction(e -> {
+            tree = new Tree(new DataElement(dataElementArray[0]));
+            currentState = 1;
+
+            treeGUI.getChildren().clear();
+            treeGUI.getChildren().add(new TreeGUI(tree.getRoot()));
+
+        });
      }
 }
